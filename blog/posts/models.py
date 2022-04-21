6d5e1c0b820071e70ee87bb1ca81e1890e2f247e
next-l1ght <roykrish9899@gmail.com>
2022-04-21 17:10:05 +0530

    first commit test.

---
+++ blog/posts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    date = models.DateField(auto_now=True)
+    image = models.ImageField(null=True, blank=True, upload_to='images')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'id': self.id})
